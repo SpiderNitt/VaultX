de2c45a867f0216b49058ea61d3175b110d19901
Utkarsh raj <167025193+Utkarshpandey0001@users.noreply.github.com>
2024-10-02 12:12:55 +0530

    Add files via upload

---
+++ src/screens/Main_Screen_Files/Main_Signup_File.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from '../Header/Header'
+import Main_Heading_Signup from '../Signup/Main_Heading/Main_Heading_Signup'
+import Main_Signup from '../Signup/Main_Signup/Main_Signup'
+
+const Singup_Main_File=()=>{
+    return(
+      <>
+        <Header/>
+        <Main_Heading_Signup/>
+        <Main_Signup/>
+      </>
+    )
+  }
+  
+  export default Singup_Main_File;
